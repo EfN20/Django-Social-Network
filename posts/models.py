879b62769ab203a7862653fdcaa12ada35c0b06f
hapacarti <hapacarti@gmail.com>
2021-04-22 18:33:40 +0600

    CRUD operations

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
+from _datetime import *
+
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.TextField(default='')
+    text = models.TextField(default='')
+    date = models.DateTimeField(default=datetime.now())
+    img = models.ImageField(upload_to='', blank=True)
