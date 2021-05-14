ba9694ccf98c029bdb70ee4c88e64b677d8c2c74
Azatkali <azatkali.16@gmail.com>
2021-05-14 09:31:05 +0600

    feat: profile page

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegistrationView, UpdateProfile, login_view, logout_view, IndexView
+from .views import RegistrationView, UpdateProfile, login_view, logout_view, IndexView, ProfileView
 
 app_name = 'users'
 
@@ -7,7 +7,8 @@ urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', login_view, name="login"),
     path('logout/', logout_view, name="logout"),
-    path('edit/', UpdateProfile.as_view(), name='profile'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('edit/', UpdateProfile.as_view(), name='profile_edit'),
     path('', IndexView.as_view(), name='main')
 ]
 
